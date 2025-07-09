e654212aeb9b78785787352a14121cbb2eca0ab2
VitalisAvisoft <vitalis@zarttech.com>
2025-07-09 09:43:39 +0100

    updated api url

--- main.tf
+++ main.tf
@@ -9,13 +9,18 @@ terraform {
 
 provider "opsgenie" {
   api_key = var.opsgenie_api_key
-  api_url = "api.opsgenie.com"
+  api_url = "https://api.opsgenie.com"
 }
 
+resource "opsgenie_alert" "etlantisalert" {
+  message     = "Example alert from Terraform"
+  description = "This alert was created via Terraform using a Team API Key."
+  priority    = "P3"
 
+  responders {
+    type = "team"
+    id   = "5f489b37-d2be-4a18-ac48-fd19864fb573"  
+  }
 
-resource "opsgenie_team" "atlantis" {
-  name        = "atlantisteam"
-  description = "This is a team created by Terraform."
-  
+  tags = ["terraform", "test"]
 }
