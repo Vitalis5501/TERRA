a984317e2cef1722790104ef7bb26b9f34f38614
VitalisAvisoft <vitalis@zarttech.com>
2025-07-09 08:01:48 +0100

    add team

--- main.tf
+++ main.tf
@@ -12,20 +12,19 @@ provider "opsgenie" {
   api_url = "https://api.opsgenie.com"
 }
 
-resource "opsgenie_team" "atlantis" {
-  name        = "atlantisteam"
-  description = "This is a team created by Terraform."
-}
-
 resource "opsgenie_user" "example_user" {
   username  = "example_user@example.com"
   full_name = "Example User"
   timezone  = "UTC"
-  role      = "User"  # Changed from user_role_id to role
+  role      = "User"
 }
 
-resource "opsgenie_team_membership" "example_membership" {
-  team_id = opsgenie_team.atlantis.id
-  user_id = opsgenie_user.example_user.id
-  role    = "user"  # Changed from "User" to "user" (lowercase)
+resource "opsgenie_team" "atlantis" {
+  name        = "atlantisteam"
+  description = "This is a team created by Terraform."
+  
+  member {
+    id   = opsgenie_user.example_user.id
+    role = "user"
+  }
 }
