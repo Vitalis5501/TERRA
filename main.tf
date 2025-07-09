eb47d275356cacfc68385b97608029debe3b72ab
VitalisAvisoft <vitalis@zarttech.com>
2025-07-09 19:38:56 +0100

    testing atlantis apply only

--- main.tf
+++ main.tf
@@ -11,34 +11,17 @@ provider "opsgenie" {
   api_key = var.opsgenie_api_key
   api_url = "api.opsgenie.com"
 }
-
-resource "opsgenie_alert_policy" "test" {
-  name               = "example policy-renamed"
-  team_id            = "5f489b37-d2be-4a18-ac48-fd19864fb573"
-  policy_description = "This is sample policy"
-  message            = "{{message}}"
-
-  time_restriction {
-    type = "weekday-and-time-of-day"
-
-    restrictions {
-      start_day  = "sunday"
-      start_hour = 21
-      start_min  = 0
-      end_day    = "monday"
-      end_hour   = 7
-      end_min    = 0
-    }
-
-    restrictions {
-      start_day  = "monday"
-      start_hour = 22
-      start_min  = 0
-      end_day    = "tuesday"
-      end_hour   = 7
-      end_min    = 0
-    }
-  }
+resource "opsgenie_schedule" "test" {
+  name        = "genieschedule-%s"
+  description = "schedule test"
+  timezone    = "Europe/Rome"
+  enabled     = false
 }
 
-
+resource "opsgenie_schedule" "atlantis_schedule" {
+  name          = "genieschedule-%s"
+  description   = "schedule test"
+  timezone      = "Europe/Rome"
+  enabled       = false
+  owner_team_id = "5f489b37-d2be-4a18-ac48-fd19864fb573"
+}
