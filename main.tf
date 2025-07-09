216192d23f3aaa0da26379bdb895e0ae72cabded
VitalisAvisoft <vitalis@zarttech.com>
2025-07-09 09:37:57 +0100

    updated api url

--- main.tf
+++ main.tf
@@ -12,19 +12,10 @@ provider "opsgenie" {
   api_url = "api.opsgenie.com"
 }
 
-resource "opsgenie_user" "example_user" {
-  username  = "example_user@example.com"
-  full_name = "Example User"
-  timezone  = "UTC"
-  role      = "User"
-}
+
 
 resource "opsgenie_team" "atlantis" {
   name        = "atlantisteam"
   description = "This is a team created by Terraform."
   
-  member {
-    id   = opsgenie_user.example_user.id
-    role = "user"
-  }
 }
