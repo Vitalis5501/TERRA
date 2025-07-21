cf3534be99f16ff9e9a42cc8eb68da64ec7694bf
VitalisAvisoft <vitalis@zarttech.com>
2025-07-21 05:55:27 +0100

    enable schedule

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "opsgenie_schedule" "test" {
   name        = "atlantisgenieschedule"
   description = "schedule test"
   timezone    = "Europe/Rome"
-  enabled     = false
+  enabled     = true
 }
 
 resource "opsgenie_schedule" "atlantis_schedule" {
