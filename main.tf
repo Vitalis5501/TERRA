05f413bd0cbde36d926da01d58ac4744b6e474b8
VitalisAvisoft <vitalis@zarttech.com>
2025-07-24 22:03:57 +0100

    transition to yaml decoder

--- main.tf
+++ main.tf
@@ -11,17 +11,26 @@ provider "opsgenie" {
   api_key = var.opsgenie_api_key
   api_url = "api.opsgenie.com"
 }
-resource "opsgenie_schedule" "test" {
-  name        = "atlantisgenieschedule_finalise"
-  description = "schedule test"
-  timezone    = "Europe/Rome"
-  enabled     = true
+
+locals {
+  schedules = yamldecode(file("${path.module}/schedules.yaml"))
 }
 
-resource "opsgenie_schedule" "atlantis_schedule" {
-  name          = "atlantis_genie_schedule"
-  description   = "schedule test"
-  timezone      = "Europe/Rome"
-  enabled       = false
-  owner_team_id = "5f489b37-d2be-4a18-ac48-fd19864fb573"
+resource "opsgenie_schedule" "dynamic_schedules" {
+  for_each = {
+    for schedule in local.schedules.schedules : schedule.name => schedule
+  }
+
+  name        = each.value.name
+  description = each.value.description
+  timezone    = each.value.timezone
+  enabled     = each.value.enabled
+
+  # Optional owner_team_id if defined
+  dynamic "owner_team_id" {
+    for_each = each.value.owner_team_id != null ? [each.value.owner_team_id] : []
+    content {
+      owner_team_id = owner_team_id.value
+    }
+  }
 }
